48386f5f4aa15249aa96bb82dbaf55ed504b7f21
Pic Aurélia <pic.aurelia@gmail.com>
2024-10-03 16:20:06 +0200

    Chicks can reduce health

--- Assets/Code/Scripts Aurélia/EnnemySpawner.cs
+++ Assets/Code/Scripts Aurélia/EnnemySpawner.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Events;
 
-
 public class EnnemySpawner : MonoBehaviour
 {
 
--- Assets/Code/Scripts Aurélia/Turrets.cs
+++ Assets/Code/Scripts Aurélia/Turrets.cs
@@ -8,12 +8,17 @@ public class Turrets : MonoBehaviour
     [Header("References")]
     [SerializeField] private Transform turretRotationPoint;
     [SerializeField] private LayerMask enemyMask;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firingPoint;
+
 
     [Header("Attribute")]
     [SerializeField] private float targetingRange = 5f;
     [SerializeField] private float rotationSpeed = 5f;
+    [SerializeField] private float bps = 1f; // Bullet Per Second
 
     private Transform target;
+    private float timeUntilFire;
 
      private void Update(){
         if (target == null) {
@@ -25,8 +30,21 @@ public class Turrets : MonoBehaviour
 
         if (!CheckTargetIsInRange()) {
             target = null;
+        } else {
+            timeUntilFire += Time.deltaTime;
+
+            if (timeUntilFire >= 1f / bps) {
+                Shoot();
+                timeUntilFire = 0f;
+            }
         }
     }
+
+        private void Shoot(){
+            GameObject bulletObj = Instantiate(bulletPrefab, firingPoint.position, Quaternion.identity);
+            Bullet bulletScript = bulletObj.GetComponent<Bullet>();
+            bulletScript.SetTarget(target);
+        }
 
         private void FindTarget(){
             RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2) transform.position, 0f, enemyMask);
